087a6bf3203e8fb96b8834934584653c23ebeab6
wJumelle <wjumelle@gmail.com>
2022-10-31 22:42:34 +0100

    [NPM] New build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'a52ebd50977ae2762716');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'bc6821434001e8aea57e');
