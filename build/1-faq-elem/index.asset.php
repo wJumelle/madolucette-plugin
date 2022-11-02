ee23c9f3f2ad4fd2690eb472c923c9730defaf22
wJumelle <wjumelle@gmail.com>
2022-11-03 00:39:21 +0100

    [FAQ ELEM] Update affichage de l'element

--- build/1-faq-elem/index.asset.php
+++ build/1-faq-elem/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '9f26fdf7bfc0f820451b');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '29e412b1110aaee4be05');
