d8cfc56234f10d7780fffffc48bcc97fabfd8071
wJumelle <wjumelle@gmail.com>
2022-11-06 18:25:02 +0100

    [FAQ] Correction responsive et BO

--- build/1-faq-elem/index.asset.php
+++ build/1-faq-elem/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '29e412b1110aaee4be05');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '9f6adedcfb5c037e9576');
